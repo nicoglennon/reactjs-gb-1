6cdc078e84a84f17382cfc46fdfdf5af892397af
Nico Glennon <nicoglennon@icloud.com>
2019-09-12 21:03:08 -0500

    move everything to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,52 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
+import './App.css'
+
+const ThemeContext = React.createContext({})
+
+const Demo = () => (
+  <ThemeContext.Consumer>
+    {({ theme, dispatch }) => (
+      <div className={`demo-background ${theme}`}>
+        <button
+          onClick={() => dispatch('TOGGLE_THEME')}
+          className="theme-button"
+        >
+          {theme}
+        </button>
+      </div>
+    )}
+  </ThemeContext.Consumer>
+)
+
+class App extends React.Component {
+  state = {
+    theme: 'light',
+  }
+
+  dispatch = action => {
+    switch (action) {
+      case 'TOGGLE_THEME':
+        return this.setState(state => ({
+          theme: state.theme === 'light' ? 'dark' : 'light',
+        }))
+      default:
+        return this.state
+    }
+  }
+
+  render() {
+    return (
+      <ThemeContext.Provider
+        value={{
+          theme: this.state.theme,
+          dispatch: this.dispatch,
+        }}
+      >
+        <Demo />
+      </ThemeContext.Provider>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
