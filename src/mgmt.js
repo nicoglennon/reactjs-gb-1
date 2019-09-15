907a0cb9e72b141d9b1728a727e0cfc49db15e74
Nico Glennon <nicoglennon@icloud.com>
2019-09-14 20:09:54 -0500

    Add MgmtConsumer

--- src/mgmt.js
+++ src/mgmt.js
@@ -1,9 +1,12 @@
 import React, { useContext, useReducer } from 'react'
 
-export const MgmtContext = React.createContext()
+const MgmtContext = React.createContext()
+
 export const MgmtProvider = ({ reducer, initialState, children }) => (
   <MgmtContext.Provider value={useReducer(reducer, initialState)}>
     {children}
   </MgmtContext.Provider>
 )
 export const useMgmt = () => useContext(MgmtContext)
+
+export const MgmtConsumer = MgmtContext.Consumer
